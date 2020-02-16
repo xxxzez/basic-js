592973f8d0d3a7ce86392225c8fa63720d323c28
Matvei Bohush <bohush.mathew@gmail.com>
2020-02-16 18:13:02 +0300

    task: count-cats complete

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,15 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(arr) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++){
+    for(let k = 0; k < arr[i].length; k++){
+      let x = arr[i][k];
+      if (x === String){
+        x.trim();
+      }
+      if (x == '^^'){
+          num++;
+      }
+    }
+  }
+  return num;
+}
