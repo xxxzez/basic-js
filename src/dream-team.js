e942143a3c50a93765428afd3271a9299a45cc01
Matvei Bohush <bohush.mathew@gmail.com>
2020-02-17 01:10:50 +0300

    task: dream-team complete

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(arr) {
+    if (Array.isArray(arr) !== true) return false;
+    let letters = [];
+    for (let i = 0; i < arr.length; i++){
+        if( typeof arr[i] === 'string'){
+        let x = arr[i].trim();
+        let y = x.slice(0,1);
+        let z = y.toUpperCase();
+        letters.push(z);
+        }    
+    }   
+    letters.sort();
+    let result = letters.join('');
+    return result;
+}
