df5c7f631fbfb8e8d642db6118ac2a4aa4f49db0
Matvei Bohush <bohush.mathew@gmail.com>
2020-02-19 00:17:42 +0300

    task: what-season 8/9

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,11 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (date === undefined) return 'Unable to determine the time of year!';
+  // if (Date.setPrototypeOf(date) !== Date) {
+  //   throw new TypeError;
+  // }
+  let myMonth = date.getMonth();
+  if (myMonth >= 5 && myMonth <= 7) return 'summer';
+  if (myMonth >= 8 && myMonth <= 10) return 'autumn';
+  if ((myMonth >= 0 && myMonth <= 1) || myMonth == 11) return 'winter';
+  if (myMonth >= 2 && myMonth <= 4) return 'spring';
+}
