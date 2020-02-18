4308b665d005f8075a88eee2f8c493dbc7332bad
Matvei Bohush <bohush.mathew@gmail.com>
2020-02-19 02:51:58 +0300

    task: what-season 9/9

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,3 @@
-module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+module.exports = class DepthCalculator {calculateDepth(arr) {
+    
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,9 +1,6 @@
 module.exports = function getSeason(date) {
   if (date === undefined) return 'Unable to determine the time of year!';
-  // if (Date.getPrototypeOf(date) !== Date) {
-  //   throw new TypeError;
-  // }
-  let myMonth = date.getMonth();
+  let myMonth = date.getUTCMonth();
   if (myMonth >= 5 && myMonth <= 7) return 'summer';
   if (myMonth >= 8 && myMonth <= 10) return 'autumn';
   if ((myMonth >= 0 && myMonth <= 1) || myMonth == 11) return 'winter';
