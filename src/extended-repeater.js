d8d03358260cecf44b6f804fd899a8b984ef0695
Matvei Bohush <bohush.mathew@gmail.com>
2020-02-19 18:32:47 +0300

    task: extended-repeater 8/9

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,28 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let myWord = '';
+    let repeatTimes = options.repeatTimes;
+    let separator = options.separator;
+    let addition = options.addition;
+    let additionRepeatTimes = options.additionRepeatTimes;
+    let additionSeparator = options.additionSeparator;
+
+    repeatTimes = repeatTimes || 1;
+    separator = separator || '+';
+    addition = addition || '';
+    additionRepeatTimes =  additionRepeatTimes || 1;
+    additionSeparator = additionSeparator || '';
+    
+    for ( let i = 0; i < repeatTimes; i++) {
+        myWord += str;
+        for(let k = 0; k < additionRepeatTimes; k++) {
+            myWord += addition;
+            if(k < additionRepeatTimes - 1) {
+                myWord += additionSeparator;
+            }
+        }
+        if(i < repeatTimes - 1) {
+            myWord += separator;
+        }
+    }
+    return myWord;
 };
-  
