73e87b16bd766e10f8102faa463430e355d7b0d4
Matvei Bohush <bohush.mathew@gmail.com>
2020-02-19 16:51:11 +0300

    task: carbon-dating 7/7

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,13 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+module.exports = function dateSample(sampleActivity) {
+  let ln2 = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+  if ( !sampleActivity ) return false;
+  if ( typeof(sampleActivity ) !== 'string') return false;
+  if ( isNaN(parseFloat(sampleActivity))) return false;
+  if ( +(sampleActivity) <= 0 || +(sampleActivity) > MODERN_ACTIVITY) return false;
+
+  let result = Math.ceil((Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity))) / (ln2/HALF_LIFE_PERIOD));
+  return result;
 };
